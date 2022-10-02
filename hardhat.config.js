f705beefdf7e350b42bed6259f03c32d39402a5c
Jacek Ostański <jacek@ivolution.pl>
2022-10-02 17:33:17 +0200

    added verification to the code.

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,4 @@
-require('@nomiclabs/hardhat-waffle');
+require("@nomiclabs/hardhat-etherscan");
 require("dotenv").config({ path: ".env" });
 
 module.exports = {
@@ -9,4 +9,9 @@ module.exports = {
       accounts: [process.env.GOERLI_PRIVATE_KEY],
     },
   },
+  etherscan: {
+    // Your API key for Etherscan
+    // Obtain one at https://etherscan.io/
+    apiKey: process.env.ETHERSCAN_API_KEY,
+  }
 };
